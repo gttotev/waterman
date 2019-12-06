19629f4873a5e281a931b39e5ee29f839f40a169
Gabriel Totev <gttotev8@gmail.com>
2019-12-05 21:48:18 -0800

    Add camera_server for web streaming

--- waterman.py
+++ waterman.py
@@ -1,24 +1,35 @@
 #!/usr/bin/env python3
-from gpiozero import DigitalInputDevice, DigitalOutputDevice
-from threading import Thread
 import time
+from threading import Thread
+import camera_server
+from gpiozero import DigitalInputDevice, DigitalOutputDevice
 
-SLEEP_TIME_SEC = 10
+POLL_TIME_SEC = 10
+PUMP_TIME_SEC = 5
+
+cam_mq = []
+cam_thread = Thread(target=camera_server.serve, args=(cam_mq,), daemon=True)
+cam_thread.start()
 
 in_pins = [18, 23, 24]
 sensors = [DigitalInputDevice(p) for p in in_pins]
-last_state = [s.value for s in sensors]
-print(last_state)
-
-def run_pump(pump):
-    pump.on()
-    time.sleep(SLEEP_TIME_SEC)
-    pump.off()
+sensor_last_state = [s.value for s in sensors]
+print(sensor_last_state)
 
 out_pins = [17, 27, 22]
 pumps = [DigitalOutputDevice(p, active_high=False) for p in out_pins]
 
+def run_pump(pump):
+    pump.on()
+    time.sleep(PUMP_TIME_SEC)
+    pump.off()
+
 while True:
+    time.sleep(POLL_TIME_SEC)
+
+    while len(cam_mq) > 0:
+        msg = cam_mq.pop()
+
     for i in range(len(in_pins)):
         if sensors[i].value and not pumps[i].value:
-            Thread(target=run_pump, args=(pumps[i],)).start()
+            Thread(target=run_pump, args=(pumps[i],), daemon=True).start()
